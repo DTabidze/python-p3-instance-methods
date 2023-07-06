eb5ba85ffa0a533e7ff7b747da2e98dc0d761f32
Davit Tabidze <davidtabidzef1@gmail.com>
2023-07-06 13:19:21 -0400

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,15 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
+    def bark(self):
+        print ("Woof!")
     pass
+
+    def sit(selt):
+        print ("The dog is sitting.")
+
+fibo = Dog()
+fibo.bark()
+
+snoopy = Dog()
+snoopy.bark()
